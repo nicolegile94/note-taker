7574009ef8d7068db130c87127d7735bd8adba1e
Nicole Gile <nicolegile94@gmail.com>
2020-09-06 16:43:18 -0500

    Fixed Heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { db } = require('./data/db/db.json');
 
@@ -8,6 +9,6 @@ app.get('/api/db', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
