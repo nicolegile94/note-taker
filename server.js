1b2202580c157a172040dc32ffd638273cfcbb53
Nicole Gile <nicolegile94@gmail.com>
2020-09-06 19:07:25 -0500

    integrate html pages

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { db } = require('./data/db/db.json');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
+const { notes } = require('./data/db/db.json');
 
-app.get('/api/db', (req, res) => {
-    let results = db;
+function createNewNote(body, noteArray) {
+    const note = body;
+    noteArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/db/db.json'),
+        JSON.stringify({ db: noteArray }, null, 2)
+    );
+
+    return note;
+}
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
     console.log(req.query)
     res.json(results);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, db);
+
+    res.json(note);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.listen(PORT, () => {
